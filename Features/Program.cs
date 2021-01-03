793b84dc9cb6f4faa3fa22c3b6b32ee2da4401a0
Tomasz B <tomaszbanach12@gmail.com>
2021-01-03 02:30:43 +0100

    Lambda method, anonymous method, named method and extension method

--- Features/Program.cs
+++ Features/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 //using System.Linq;
 
 namespace Features
@@ -8,24 +9,57 @@ namespace Features
     {
         static void Main(string[] args)
         {
-            IEnumerable<Employee> developers = new Employee[]
-            {
-                new Employee{Id = 1, Name = "Jeff"},
-                new Employee{Id = 1, Name = "Bob"}
-            };
-
+            Console.WriteLine("****Enumerator****");
             IEnumerable<Employee> sales = new List<Employee>()
             {
                 new Employee{Id = 1, Name = "Chris" },
-                new Employee{Id = 1, Name = "Paul" }
+                new Employee{Id = 2, Name = "Paul" },
+                new Employee{Id = 3, Name = "John" },
+                new Employee{Id = 4, Name = "Ross" },
+                new Employee{Id = 5, Name = "Liam" },
+                new Employee{Id = 6, Name = "Noah" },
+                new Employee{Id = 7, Name = "Todd" },
+                new Employee{Id = 8, Name = "Bob"}
             };
-            Console.WriteLine(sales.Count());
-
-            IEnumerator<Employee> enumerator = developers.GetEnumerator();
+            // Enumerator
+            IEnumerator<Employee> enumerator = sales.GetEnumerator();
             while (enumerator.MoveNext())
             {
                 Console.WriteLine(enumerator.Current.Name);
             }
+
+            Console.WriteLine("****Extension method****");
+            // Extension method
+            Console.WriteLine(sales.Count());
+
+            Console.WriteLine("****Named method****");
+            // Named method
+            foreach (var item in sales.Where(NameStatsWithR))
+            {
+                Console.WriteLine(item.Name);
+            }
+
+            Console.WriteLine("****Anonymous method****");
+            // Anonymous method
+            foreach (var item in sales.Where(
+                delegate (Employee employee)
+                {
+                    return employee.Name.StartsWith('J');
+                }))
+            {
+                Console.WriteLine(item.Name);
+            }
+
+            Console.WriteLine("****Lambda method****");
+            // Lambda method
+            foreach (var item in sales.Where(e => e.Name.StartsWith('L')))
+            {
+                Console.WriteLine(item.Name);
+            }
+        }
+        private static bool NameStatsWithR(Employee employee)
+        {
+            return employee.Name.StartsWith('R');
         }
     }
 }
