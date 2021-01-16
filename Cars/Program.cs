16d2b7000c902ce0d94ae2ac179b5ce9e25342fb
Tomasz B <tomaszbanach12@gmail.com>
2021-01-16 16:56:10 +0100

    End of chapter 5

--- Cars/Program.cs
+++ Cars/Program.cs
@@ -9,18 +9,18 @@ namespace Cars
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("****Show 30 car names with extension method syntax****");
+            Console.WriteLine("****Show 15 car names with extension method syntax****");
             var cars = ProcessFileByExtensionMethod("fuel.csv");
 
-            foreach (var car in cars.Take(30))
+            foreach (var car in cars.Take(15))
             {
                 Console.WriteLine(car.Name);
             }
 
-            Console.WriteLine("****Show 30 car names with query method syntax****");
+            Console.WriteLine("****Show 15 car names with query method syntax****");
             cars = ProcessFileByQueryMethod("fuel.csv");
 
-            foreach (var car in cars.Take(30))
+            foreach (var car in cars.Take(15))
             {
                 Console.WriteLine(car.Name);
             }
@@ -54,7 +54,7 @@ namespace Cars
 
             foreach (var car in query.Take(10))
             {
-                Console.WriteLine($"{car.Name}: {car.Combined}");
+                Console.WriteLine($"{car.Manufacturer} {car.Name}: {car.Combined}");
             }
 
             Console.WriteLine("****Finding the top 10 most fuel efficent cars by manufacturer and year with query method syntax****");
@@ -66,7 +66,7 @@ namespace Cars
 
             foreach (var car in query.Take(10))
             {
-                Console.WriteLine($"{car.Name}: {car.Combined}");
+                Console.WriteLine($"{car.Manufacturer} {car.Name}: {car.Combined}");
             }
 
             Console.WriteLine("****Finding the best most fuel efficent car by manufacturer and year with extension method syntax****");
@@ -80,27 +80,82 @@ namespace Cars
 
             Console.WriteLine("****Is there any car manufactured by Ford****");
             var isCarManufacturedByFord = cars.Any(cars => cars.Manufacturer == "Ford");
-
             Console.WriteLine(isCarManufacturedByFord);
 
             Console.WriteLine("****Is there every car manufactured by Ford****");
             var isEveryCarManufacturedByFord = cars.All(cars => cars.Manufacturer == "Ford");
-
             Console.WriteLine(isEveryCarManufacturedByFord);
 
             Console.WriteLine("****Is there any car manufactured by BMW and produced in 2016****");
             var isCarBMWProducedIn2016 = cars.Contains(selectedCar);
-
             Console.WriteLine(isCarBMWProducedIn2016);
+
+            Console.WriteLine("****Show 15 car names with custom extension method ToCar****");
+            cars = ProcessFileByExtensionMethodCalledToCar("fuel.csv");
+
+            foreach (var car in cars.Take(15))
+            {
+                Console.WriteLine(car.Name);
+            }
+
+            Console.WriteLine("****Finding the top 10 most fuel efficent cars by manufacturer and year by projection with anonymous type****");
+            var queryFromProjection =
+                    from car in cars
+                    where car.Manufacturer == "BMW" && car.Year == 2016
+                    orderby car.Combined descending, car.Name ascending
+                    select new
+                    {
+                        car.Manufacturer,
+                        car.Name,
+                        car.Combined
+                    };
+
+            foreach (var car in queryFromProjection.Take(10))
+            {
+                Console.WriteLine($"{car.Manufacturer} {car.Name}: {car.Combined}");
+            }
+
+            Console.WriteLine("****Show 15 car names by projection with anonymous type****");
+            queryFromProjection =
+                cars.Select(c => new
+                {
+                    c.Manufacturer,
+                    c.Name,
+                    c.Combined
+                });
+
+            foreach (var car in query.Take(15))
+            {
+                Console.WriteLine($"{car.Manufacturer} {car.Name}: {car.Combined}");
+            }
+
+            Console.WriteLine("****Show 2 car names and split it by characters by two foreach loops****");
+            var queryTwoLoops = cars.Select(c => c.Name);
+
+            foreach (var name in queryTwoLoops.Take(2))
+            {
+                foreach (var character in name)
+                {
+                    Console.WriteLine(character);
+                }
+            }
+
+            Console.WriteLine("****Show 2 car names and split it by characters by flat data with SelectMany****");
+            var querySelectMany = cars.Take(2).SelectMany(c => c.Name);
+
+            foreach (var character in querySelectMany)
+            {
+                Console.WriteLine(character);
+            }
         }
         private static List<Car> ProcessFileByExtensionMethod(string path)
         {
-            return
-                File.ReadAllLines(path)
-                    .Skip(1)
-                    .Where(l => l.Length > 1)
-                    .Select(Car.ParseFromCsv)
-                    .ToList();
+            var query = File.ReadAllLines(path)
+                            .Skip(1)
+                            .Where(l => l.Length > 1)
+                            .Select(Car.ParseFromCsv);
+
+            return query.ToList();
         }
         private static List<Car> ProcessFileByQueryMethod(string path)
         {
@@ -108,26 +163,17 @@ namespace Cars
                         .Skip(1)
                         where l.Length > 1
                         select Car.ParseFromCsv(l);
+
             return query.ToList();
         } 
-        private static List<Car> ProcessFileByExtensionMethodMostFuelEfficent(string path)
+        private static List<Car> ProcessFileByExtensionMethodCalledToCar(string path)
         {
-            return
+            var query =
                 File.ReadAllLines(path)
                     .Skip(1)
                     .Where(l => l.Length > 1)
-                    .Select(Car.ParseFromCsv)
-                    .OrderByDescending(c => c.Combined)
-                    .ThenBy(c => c.Name).Take(10)
-                    .ToList();
-        }
-        private static List<Car> ProcessFileByQueryMethodMostFuelEfficent(string path)
-        {
-            var query = from l in File.ReadAllLines(path)
-                        .Skip(1)
-                        .Take(30)
-                        where l.Length > 1
-                        select Car.ParseFromCsv(l);
+                    .ToCar();
+
             return query.ToList();
         }
     }
