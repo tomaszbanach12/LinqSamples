945476ec6f106c37053d41aa11c1f21445a53b49
Tomasz B <tomaszbanach12@gmail.com>
2021-04-09 16:14:58 +0200

    End of chapter 7, working with XElement, XAttibute and XNamespace

--- Cars/XmlMethods.cs
+++ Cars/XmlMethods.cs
@@ -7,17 +7,6 @@ namespace Cars
 {
     public static class XmlMethods
     {
-        public static void GetXmlAttributeByQueryMethod(string path, string manufacturer)
-        {
-            var document = XDocument.Load(path);
-
-            var query =
-                from element in document.Element("Cars").Elements("Car")
-                where element.Attribute("Manufacturer").Value == manufacturer
-                select element.Attribute("Name");
-        }
-
-
         public static void CreateXmlByForeachMethod(List<Car> records, string path)
         {
             var document = new XDocument();
@@ -66,6 +55,64 @@ namespace Cars
 
             document.Add(cars);
             document.Save(path);
+        }
+
+        public static void CreateXmlWithNamespaceByExtensionMethod(List<Car> records, string path, string nsString, string exString)
+        {
+            var ns = (XNamespace)nsString;
+            var ex = (XNamespace)exString;
+            var document = new XDocument();
+            var cars = new XElement(ns + "Cars");
+
+            var elements = records.Select(record => new XElement(ex + "Car",
+                                                             new XAttribute("Name", record.Name),
+                                                             new XAttribute("Combined", record.Combined),
+                                                             new XAttribute("Manufacturer", record.Manufacturer)));
+
+
+            cars.Add(elements);
+            cars.Add(new XAttribute(XNamespace.Xmlns + "ex", ex));
+
+            document.Add(cars);
+            document.Save(path);
+        }
+
+        public static IEnumerable<XAttribute> GetXmlAttributesByElementsUsingQueryMethod(string path, string manufacturer)
+        {
+            var document = XDocument.Load(path);
+
+            var query =
+                from element in document.Element("Cars").Elements("Car")
+                where element.Attribute("Manufacturer")?.Value == manufacturer
+                select element.Attribute("Name");
+
+            return query;
+        }
+
+        public static IEnumerable<XAttribute> GetXmlAttributesByDescendantsUsingQueryMethod(string path, string manufacturer)
+        {
+            var document = XDocument.Load(path);
+
+            var query =
+                from element in document.Descendants("Car")
+                where element.Attribute("Manufacturer")?.Value == manufacturer
+                select element.Attribute("Name");
+
+            return query;
+        }
+
+        public static IEnumerable<XAttribute> GetXmlAttributesByElementsByNamespaceUsingQueryMethod(string path, string manufacturer, string nsString, string exString)
+        {
+            var ns = (XNamespace)nsString;
+            var ex = (XNamespace)exString;
+            var document = XDocument.Load(path);
+
+            var query =
+                from element in document.Element(ns + "Cars")?.Elements(ex + "Car") ?? Enumerable.Empty<XElement>()
+                where element.Attribute("Manufacturer")?.Value == manufacturer
+                select element.Attribute("Name");
+
+            return query;
         }
     }
 }
--- Cars/XmlProgram.cs
+++ Cars/XmlProgram.cs
@@ -16,17 +16,45 @@ namespace Cars
 
             Console.WriteLine("****Build element oriented xml file with functional construction (foreach version)****");
             var fileName = "fuel.xml";
-            XmlMethods.CreateXmlByExtensionMethod(records, fileName);
+            XmlMethods.CreateXmlByForeachMethod(records, fileName);
 
             Console.WriteLine("****Build element oriented xml file with functional construction (query method version)****");
-            var fileName2 = "fuel_by_query_method.xml";
-            XmlMethods.CreateXmlByQueryMethod(records, fileName2);
+            fileName = "fuel_by_query_method.xml";
+            XmlMethods.CreateXmlByQueryMethod(records, fileName);
 
             Console.WriteLine("****Build element oriented xml file with functional construction (extension method version)****");
-            var fileName3 = "fuel_by_extension_method.xml";
-            XmlMethods.CreateXmlByExtensionMethod(records, fileName3);
+            fileName = "fuel_by_extension_method.xml";
+            XmlMethods.CreateXmlByExtensionMethod(records, fileName);
+            
+            Console.WriteLine("****Build element oriented xml file with functional construction and namespace (extension method version)****");
+            var ns = "http://github.com/cars/2021";
+            var ex = "http://github.com/cars/2021/ex";
+            fileName = "fuel_with_namespace_by_extension_method.xml";
+            XmlMethods.CreateXmlWithNamespaceByExtensionMethod(records, fileName, ns, ex);
 
-            var document = XDocument.Load(fileName);
+            Console.WriteLine("****Get xml attribute by elements and namespace where manufacturer is BMW (query method version)****");
+            fileName = "fuel_with_namespace_by_extension_method.xml";
+            var xAttributes = XmlMethods.GetXmlAttributesByElementsByNamespaceUsingQueryMethod(fileName, "BMW", ns, ex);
+            foreach (var name in xAttributes)
+            {
+                Console.WriteLine(name);
+            }
+
+            Console.WriteLine("****Get xml attribute by elements where manufacturer is BMW (query method version)****");
+            fileName = "fuel.xml";
+            xAttributes = XmlMethods.GetXmlAttributesByElementsUsingQueryMethod(fileName, "BMW");
+            foreach (var name in xAttributes)
+            {
+                Console.WriteLine(name);
+            }
+
+            Console.WriteLine("****Get xml attribute by descendants where manufacturer is BMW (query method version)****");
+            fileName = "fuel.xml";
+            xAttributes = XmlMethods.GetXmlAttributesByDescendantsUsingQueryMethod(fileName, "BMW");
+            foreach (var name in xAttributes)
+            {
+                Console.WriteLine(name);
+            }
         }
     }
 }
